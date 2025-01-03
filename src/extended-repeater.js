7d3c59068c85726cff2f95dbd3a2bd406b7b7559
Alexey Inshakov <alexeyinshakov992@gmail.com>
2025-01-03 11:05:29 +0300

    83 passing

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = '';
+  let { repeatTimes, separator, addition, additionRepeatTimes, additionSeparator } = options;
+  let additionStr = '';
+
+  if (!separator) {
+    separator = '+'
+  }
+
+  if (!additionSeparator) {
+    additionSeparator = '|'
+  }
+
+  if (!repeatTimes) {
+    repeatTimes = 1;
+  }
+
+  if (!additionRepeatTimes) {
+    additionRepeatTimes = 1;
+  }
+
+  if (addition || addition === false || addition === null) {
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      additionStr += addition;
+      if (i !== additionRepeatTimes - 1) {
+        additionStr += additionSeparator;
+      }
+    }
+  }
+
+  for (let i = 0; i < repeatTimes; i++) {
+    result += str + additionStr;
+
+    if (i !== repeatTimes - 1) {
+      result += separator;
+    }
+
+  }
+
+  return result;
 }
+
 
 module.exports = {
   repeater
